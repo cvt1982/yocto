9b59948c2902bedbd384364b2ba6f48ebbbb2471
cvt <->
2017-03-09 19:50:03 +0100

    Minor tweak for the install.sh script

--- install.sh
+++ install.sh
@@ -43,3 +43,4 @@ ln -s $YOCTO_ROOT/yocto-conf/conf/local.conf    build/conf/local.conf
 ln -s $YOCTO_ROOT/yocto-conf/conf/site.conf     build/conf/site.conf
 
 source oe-init-build-env
+cd build/conf
