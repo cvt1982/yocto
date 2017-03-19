caf8bd6f7d68ff4e00784d889c65cf0db6d0189d
cvt <->
2017-03-19 13:51:11 +0100

    Add install-morty.sh script for 'morty' branch installations

---
+++ install-morty.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Abort on error
+set -e
+
+ORG=$(pwd)
+BRANCH=morty
+
+# Get yocto base repository
+git clone git://git.yoctoproject.org/poky
+cd poky
+git checkout $BRANCH
+
+# Save the root folder of the repository
+YOCTO_ROOT=$(pwd)
+
+git clone git://git.yoctoproject.org/meta-raspberrypi
+cd meta-raspberrypi
+git checkout $BRANCH
+
+cd $YOCTO_ROOT
+
+git clone git://git.yoctoproject.org/meta-mono
+cd meta-mono
+git checkout $BRANCH
+
+cd $YOCTO_ROOT
+
+git clone git://git.openembedded.org/meta-openembedded
+cd meta-openembedded
+git checkout $BRANCH
+
+cd $YOCTO_ROOT
+
+git clone https://github.com/cvt1982/yocto-meta.git
+git clone https://github.com/cvt1982/yocto-conf.git
+
+cd $YOCTO_ROOT
+
+# Create symlinks to build/conf
+mkdir -p build/conf
+ln -s $YOCTO_ROOT/yocto-conf/conf/bblayers.conf build/conf/bblayers.conf
+ln -s $YOCTO_ROOT/yocto-conf/conf/local.conf    build/conf/local.conf
+ln -s $YOCTO_ROOT/yocto-conf/conf/site.conf     build/conf/site.conf
+
+source oe-init-build-env
